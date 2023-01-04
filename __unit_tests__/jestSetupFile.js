b05f716ff9b4b5df9b16941d52d979409bdbeaaa
cied <cied@cieds-MacBook-Air.local>
2023-01-04 08:35:20 +0530

    refactor: rename folders and delete unwanted folders

--- __unit_tests__/jestSetupFile.js
+++
@@ -1,7 +0,0 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-import mockNetInfo from '@react-native-community/netinfo/jest/netinfo-mock';
-import mockDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
-jest.mock('@react-native-community/netinfo', () => mockNetInfo);
-jest.mock('react-native-device-info', () => mockDeviceInfo);
